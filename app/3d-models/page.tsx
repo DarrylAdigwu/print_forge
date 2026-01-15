429afdfbbee0f668f17516e081d5fa2bc9abec44
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2026-01-15 12:11:43 -0800

    added searh bar and filtering

--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -1,7 +1,28 @@
 import ModelsGrid from "@/app/components/ModelsGrid"
+import type { ModelsPageProps } from "@/app/types"
 import { getModels } from "@/app/lib/models"
+import Form from "next/form"
 
-export default async function Page() {
-    const models = await getModels()
-    return <ModelsGrid title="3D Models" models={models} />
+export default async function Page({ searchParams }: ModelsPageProps) {
+  const { search } = await searchParams;
+  const models = await getModels()
+  const filterModels = search ? models.filter((model) => {
+    return model.description.toLowerCase().includes(search?.toLowerCase()) ||
+      model.name.toLowerCase().includes(search?.toLowerCase())
+  }) : models
+  console.log(filterModels)
+  return (
+    <>
+      <Form action="/3d-models" className="w-full px-5 md:px-0 md:max-w-xl">
+        <input
+        type="text"
+          name="search"
+          id="search"
+          placeholder="Search models"defaultValue={search}
+          className="w-full py-3 pl-5 pr-5 text-sm placeholder-gray-500 bg-white border border-[#606060] rounded-full focus:border-[#606060] focus:outline-none focus:ring-0 md:text-base"
+        />
+      </Form>
+      <ModelsGrid title="3D Models" models={filterModels} />
+    </>
+  )
 }
